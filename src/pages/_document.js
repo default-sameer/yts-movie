bed32f6ff3e632ebb327d479e8f5e4298277c9bb
default-sameer <sameerjoshicr7@gmail.com>
2022-04-19 11:01:00 +0545

    🚀 Standalone PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,13 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <meta name="theme-color" content="#ffbd59" />
                 </Head>
                 <body>
                     <Main />
